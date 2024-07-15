ae564365c5cbe3891e9122bc7a9037219000f545
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2024-07-15 16:35:03 -0300

    Feat: Adiciona exercícios da Aula 5

--- examples/animation.js
+++ examples/animation.js
@@ -1,17 +1,19 @@
-import * as THREE from  'three';
-import Stats from '../build/jsm/libs/stats.module.js';
-import GUI from '../libs/util/dat.gui.module.js'
-import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
-import {initRenderer, 
-        initCamera, 
-        onWindowResize,
-        initDefaultSpotlight} from "../libs/util/util.js";
+import * as THREE from "three";
+import Stats from "../build/jsm/libs/stats.module.js";
+import GUI from "../libs/util/dat.gui.module.js";
+import { TrackballControls } from "../build/jsm/controls/TrackballControls.js";
+import {
+  initRenderer,
+  initCamera,
+  onWindowResize,
+  initDefaultSpotlight,
+} from "../libs/util/util.js";
 
-var stats = new Stats();          // To show FPS information
-var scene = new THREE.Scene();    // Create main scene
-var renderer = initRenderer();    // View function in util/utils
+var stats = new Stats(); // To show FPS information
+var scene = new THREE.Scene(); // Create main scene
+var renderer = initRenderer(); // View function in util/utils
 var camera = initCamera(new THREE.Vector3(5, 5, 7)); // Init camera in this position
-var trackballControls = new TrackballControls( camera, renderer.domElement );
+var trackballControls = new TrackballControls(camera, renderer.domElement);
 initDefaultSpotlight(scene, new THREE.Vector3(2, 4, 2)); // Use default light
 
 // Set angles of rotation
@@ -21,52 +23,63 @@ var speed = 0.05;
 var animationOn = true; // control if animation is on or of
 
 // Show world axes
-var axesHelper = new THREE.AxesHelper( 12 );
-scene.add( axesHelper );
+var axesHelper = new THREE.AxesHelper(12);
+scene.add(axesHelper);
 
 // Base sphere
 var sphereGeometry = new THREE.SphereGeometry(0.2, 32, 32);
-var sphereMaterial = new THREE.MeshPhongMaterial(
-    {color:'rgb(180,180,255)', shininess:"40", specular:'rgb(255,255,255'} );
-var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+var sphereMaterial = new THREE.MeshPhongMaterial({
+  color: "rgb(180,180,255)",
+  shininess: "40",
+  specular: "rgb(255,255,255)",
+});
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 // Set initial position of the sphere
 sphere.translateX(1.0).translateY(1.0).translateZ(1.0);
 
 // More information about cylinderGeometry here ---> https://threejs.org/docs/#api/en/geometries/CylinderGeometry
 var cylinderGeometry = new THREE.CylinderGeometry(0.1, 0.1, 2.0, 25);
-var cylinderMaterial = new THREE.MeshPhongMaterial( {color:'rgb(100,255,100)'});
-var cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+var cylinderMaterial = new THREE.MeshPhongMaterial({
+  color: "rgb(100,255,100)",
+});
+var cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
 sphere.add(cylinder);
 
 // Rede cylinder
 var cylinderGeometry2 = new THREE.CylinderGeometry(0.07, 0.07, 1.0, 25);
-var cylinderMaterial2 = new THREE.MeshLambertMaterial( {color:'rgb(255,100,100)'} );
-var cylinder2 = new THREE.Mesh( cylinderGeometry2, cylinderMaterial2 );
+var cylinderMaterial2 = new THREE.MeshLambertMaterial({
+  color: "rgb(255,100,100)",
+});
+var cylinder2 = new THREE.Mesh(cylinderGeometry2, cylinderMaterial2);
 cylinder.add(cylinder2);
 
 // Listen window size changes
-window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+window.addEventListener(
+  "resize",
+  function () {
+    onWindowResize(camera, renderer);
+  },
+  false
+);
 
 buildInterface();
 render();
 
-function rotateCylinder()
-{
+function rotateCylinder() {
   // More info:
   // https://threejs.org/docs/#manual/en/introduction/Matrix-transformations
   cylinder.matrixAutoUpdate = false;
   cylinder2.matrixAutoUpdate = false;
 
   // Set angle's animation speed
-  if(animationOn)
-  {
-    angle+=speed;
-    angle2+=speed*2;
-    
+  if (animationOn) {
+    angle += speed;
+    angle2 += speed * 2;
+
     var mat4 = new THREE.Matrix4();
-    cylinder.matrix.identity();  // reset matrix
-    cylinder2.matrix.identity();  // reset
+    cylinder.matrix.identity(); // reset matrix
+    cylinder2.matrix.identity(); // reset
 
     // Will execute T1 and then R1
     cylinder.matrix.multiply(mat4.makeRotationZ(angle)); // R1
@@ -79,33 +92,33 @@ function rotateCylinder()
   }
 }
 
-function buildInterface()
-{
-  var controls = new function ()
-  {
-    this.onChangeAnimation = function(){
+function buildInterface() {
+  var controls = new (function () {
+    this.onChangeAnimation = function () {
       animationOn = !animationOn;
     };
     this.speed = 0.05;
 
-    this.changeSpeed = function(){
+    this.changeSpeed = function () {
       speed = this.speed;
     };
-  };
+  })();
 
   // GUI interface
   var gui = new GUI();
-  gui.add(controls, 'onChangeAnimation',true).name("Animation On/Off");
-  gui.add(controls, 'speed', 0.05, 0.5)
-    .onChange(function(e) { controls.changeSpeed() })
+  gui.add(controls, "onChangeAnimation", true).name("Animation On/Off");
+  gui
+    .add(controls, "speed", 0.05, 0.5)
+    .onChange(function (e) {
+      controls.changeSpeed();
+    })
     .name("Change Speed");
 }
 
-function render()
-{
+function render() {
   stats.update(); // Update FPS
   trackballControls.update();
   rotateCylinder();
   requestAnimationFrame(render);
-  renderer.render(scene, camera) // Render scene
+  renderer.render(scene, camera); // Render scene
 }
---
+++ exercises/Aula 2/Exercicio01.js
@@ -0,0 +1,94 @@
+import * as THREE from "three";
+import { OrbitControls } from "../../build/jsm/controls/OrbitControls.js";
+import {
+  initRenderer,
+  initCamera,
+  initDefaultBasicLight,
+  setDefaultMaterial,
+  InfoBox,
+  onWindowResize,
+  createGroundPlaneXZ,
+} from "../../libs/util/util.js";
+
+let scene, renderer, camera, material, light, orbit; // Initial variables
+scene = new THREE.Scene(); // Create main scene
+renderer = initRenderer(); // Init a basic renderer
+camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
+material = setDefaultMaterial(); // create a basic material
+light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
+
+// Listen window size changes
+window.addEventListener(
+  "resize",
+  function () {
+    onWindowResize(camera, renderer);
+  },
+  false
+);
+
+// Show axes (parameter is size of each axis)
+let axesHelper = new THREE.AxesHelper(12);
+scene.add(axesHelper);
+
+// create the ground plane
+let plane = createGroundPlaneXZ(20, 20);
+scene.add(plane);
+
+//////////////////////////////////  EXERCÍCIOS 01 //////////////////////////////////
+
+const defaultHeight = 1.5;
+
+// create a cube
+let cubeGeometry = new THREE.BoxGeometry(11, 0.3, 6);
+let cube = new THREE.Mesh(cubeGeometry, material);
+// position the cube
+cube.position.set(0.0, defaultHeight * 2, 0.0);
+// add the cube to the scene
+scene.add(cube);
+
+const cylinderGeometry = new THREE.CylinderGeometry(0.2, 0.2, 3);
+
+createTableFeet();
+
+function createTableFeet() {
+  let footXPosition = -5,
+    footZPosition;
+
+  for (let i = 0; i < 2; i++) {
+    footZPosition = -1;
+
+    let cylinder = new THREE.Mesh(cylinderGeometry, material);
+    cylinder.position.set(0.0, defaultHeight, 0.0);
+
+    cylinder.translateX(footXPosition);
+
+    for (let j = 0; j < 2; j++) {
+      cylinder.translateZ(footZPosition);
+      console.log(cylinder.position);
+      scene.add(cylinder);
+
+      setTimeout(() => (footZPosition = 1), 500);
+    }
+
+    footXPosition = 5;
+  }
+}
+
+////////////////////////////////  FIM EXERCÍCIOS 01 ////////////////////////////////
+
+// Use this to show information onscreen
+let controls = new InfoBox();
+controls.add("Basic Scene");
+controls.addParagraph();
+controls.add("Use mouse to interact:");
+controls.add("* Left button to rotate");
+controls.add("* Right button to translate (pan)");
+controls.add("* Scroll to zoom in/out.");
+controls.show();
+
+render();
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera); // Render scene
+}
